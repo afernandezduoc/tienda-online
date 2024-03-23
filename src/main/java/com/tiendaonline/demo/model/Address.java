d82c7304be2254765a27d1604eff6911902b78ad
Alejandro Fernández <alej.fernandez@duocuc.cl>
2024-03-23 13:02:55 -0300

    Primer commit: Creación de proyecto Spring Boot, además se agregan los model

---
+++ src/main/java/com/tiendaonline/demo/model/Address.java
@@ -0,0 +1,49 @@
+package com.tiendaonline.demo.model;
+
+public class Address {
+    private Integer id;
+    private String street;
+    private String city;
+    private String country;
+
+    // Constructor
+    public Address(Integer id, String street, String city, String country) {
+        this.id = id;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+    
+    // Getter and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ src/main/java/com/tiendaonline/demo/model/Role.java
@@ -0,0 +1,7 @@
+package com.tiendaonline.demo.model;
+
+public enum Role {
+    ADMIN,
+    USER,
+    GUEST
+}
