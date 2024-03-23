a8387e22c6843c4967dc04071d642a710e22253b
Alejandro Fernández <alej.fernandez@duocuc.cl>
2024-03-23 13:48:40 -0300

    Se modifica User.java para eliminar el uso de password ya que ya no se hará un login. Se agrega inMemoryDatabase para la carga de datos de prueba

--- src/main/java/com/tiendaonline/demo/model/User.java
+++ src/main/java/com/tiendaonline/demo/model/User.java
@@ -5,16 +5,14 @@ public class User {
     private Integer id;
     private String name;
     private String email;
-    private String password;
     private Role role;
     private Address address;
 
     // Constructor
-    public User(Integer id, String name, String email, String password, Address address) {
+    public User(Integer id, String name, String email, Role guest, Address address) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.password = password;
         this.address = address;
     }
     
@@ -41,14 +39,6 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public Role getRole() {
